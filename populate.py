469e9bf15cf96f77ef8327ebf8b5dfe71e2dd29e
Peter Rabai <2394958r@student.gla.ac.uk>
2020-03-11 17:59:02 +0000

    populate script for meals

--- populate.py
+++ populate.py
@@ -2,60 +2,37 @@ import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'team_project_food_cloud.settings')
 
 import django
+import random
 django.setup()
-from food_cloud.models import Category, Page
+from food_cloud.models import *
 
 def populate():
-	# First, we will create lists of dictionaries containing the pages
-	# we want to add into each category.
-	# Then we will create a dictionary of dictionaries for our categories.
-	# This might seem a little bit confusing, but it allows us to iterate
-	# through each data structure, and add the data to our models.
 
-	python_pages = [
-		{'title': 'Official Python Tutorial', 'url':'http://docs.python.org/3/tutorial/'},
-		{'title':'How to Think like a Computer Scientist', 'url':'http://www.greenteapress.com/thinkpython/'},
-		{'title':'Learn Python in 10 Minutes', 'url':'http://www.korokithakis.net/tutorials/python/'} ]
+	meals = {'Test_meal_1': {'description': "Test_meal_1 description", 'price': 15, 'restaurant_id': 0, 'picture': "static/images/meal_default.jpg"}, 
+			'Test_meal_2': {'description': "Test_meal_2 description", 'price': 15, 'restaurant_id': 0, 'picture': "static/images/meal_default.jpg"},
+			'Test_meal_3': {'description': "Test_meal_3 description", 'price': 15, 'restaurant_id': 0, 'picture': "static/images/meal_default.jpg"} }
 
-	django_pages = [
-		{'title':'Official Django Tutorial', 'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
-		{'title':'Django Rocks', 'url':'http://www.djangorocks.com/'},
-		{'title':'How to Tango with Django', 'url':'http://www.tangowithdjango.com/'} ]
 
-	other_pages = [
-		{'title':'Bottle', 'url':'http://bottlepy.org/docs/dev/'},
-		{'title':'Flask', 'url':'http://flask.pocoo.org'} ]
+	for cat, cat_data in meals.items():
+		c = add_meal(cat, cat_data['description'], cat_data['price'], cat_data['restaurant_id'], cat_data['picture'])
 
-	cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64}, 
-			'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
-			'Other Frameworks': {'pages': other_pages, 'views': 0, 'likes': 0} }
 
-	# If you want to add more categories or pages,
-	# add them to the dictionaries above.
-
-	# The code below goes through the cats dictionary, then adds each category,
-	# and then adds all the associated pages for that category.
-	for cat, cat_data in cats.items():
-		c = add_cat(cat, cat_data['views'], cat_data['likes'])
-		for p in cat_data['pages']:
-			add_page(c, p['title'], p['url'])
-
-	# Print out the categories we have added.
-	for c in Category.objects.all():
-		for p in Page.objects.filter(category=c):
-			print(f'- {c}: {p}')
-
-def add_page(cat, title, url, views=0):
-	p = Page.objects.get_or_create(category=cat, title=title)[0]
-	p.url=url
-	p.views=views
-	p.save()
-	return p
-	
-def add_cat(name, views=0, likes=0):
-	c = Category.objects.get_or_create(name=name, views=views, likes=likes)[0]
+def add_meal(meal_name, description, price, restaurant_id, picture):
+	c = Meal.objects.get_or_create(meal_id=random.randrange(1000,10000,1), meal_name=meal_name, description=description, price=price, restaurant_id=restaurant_id, picture=picture)[0]
 	c.save()
 	return c
+			
+# def add_page(cat, title, url, views=0):
+	# p = Page.objects.get_or_create(category=cat, title=title)[0]
+	# p.url=url
+	# p.views=views
+	# p.save()
+	# return p
+	
+# def add_cat(name, views=0, likes=0):
+	# c = Category.objects.get_or_create(name=name, views=views, likes=likes)[0]
+	# c.save()
+	# return c
 	
 # Start execution here!
 if __name__ == '__main__':
