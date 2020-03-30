7eaedc7d33a0639f6dc5ddd80159d89ce01a8498
Olalekan Akala <2514445a@student.gla.ac.uk>
2020-03-29 22:21:19 -0400

    Added signal for after deleting meals

--- food_cloud/signals.py
+++ food_cloud/signals.py
@@ -7,7 +7,7 @@ def check_product_description(sender, instance, **kwargs):
     if not instance.description:
         instance.description = 'No Description Has Been Added'
 
-@receiver(signals.post_save, sender=Meal)
+@receiver([signals.post_save, signals.post_delete], sender=Meal)
 def update_restaurant(sender, instance, **kwargs):
 		restaurant = RestaurantProfile.objects.get(slug = instance.restaurant_slug)
 		restaurant.save()
