0d41cad71f6cac795a0346dc2b5fb295aa5db29f
Krasimir Ivanov <krasimir2.ivanov@abv.bg>
2020-03-31 13:14:50 +0300

    Order logic implemented

--- food_cloud/models.py
+++ food_cloud/models.py
@@ -1,89 +1,116 @@
 from django.db import models
 from django.template.defaultfilters import slugify
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 import uuid
 
-class UserProfile(models.Model):
-	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	email_address = models.EmailField()
-	picture = models.ImageField(upload_to='profile_images', blank=True)
-	isCompany = models.BooleanField(default=False)
-	restaurant_id = models.IntegerField(default=0)
-	def __str__(self):
-		return self.user.username
-		
-class Meal(models.Model):
-	meal_id = models.UUIDField(max_length=10, primary_key=True, default=uuid.uuid4, blank=True)
-	meal_name = models.CharField(max_length=30, unique=True)
-	description = models.CharField(max_length=200, unique=False)
-	price = models.IntegerField(default=0)
-	restaurant_id = models.IntegerField(default=0)
-	picture = models.ImageField(upload_to='profile_images', blank=True)
-	
-	def save(self, *args, **kwargs):
-		self.slug = slugify(self.meal_name)
-		while not self.meal_id:
-			newid = ''.join([
-				random.sample(string.letters, 2),
-				random.sample(string.digits, 2),
-				random.sample(string.letters, 2),
-			])
 
-			if not self.objects.filter(pk=newid).exists():
-				self.meal_id = newid
-		super(Meal, self).save(*args, **kwargs)
-		
-	class Meta:
-		verbose_name_plural = 'Meals'
-	
-	def __str__(self):
-		return self.meal_name
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email_address = models.EmailField()
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+    isCompany = models.BooleanField(default=False)
+    restaurant_id = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
+
+
+class Meal(models.Model):
+    meal_id = models.UUIDField(
+        max_length=10, primary_key=True, default=uuid.uuid4, blank=True)
+    meal_name = models.CharField(max_length=30, unique=True)
+    description = models.CharField(max_length=200, unique=False)
+    price = models.IntegerField(default=0)
+    restaurant_id = models.IntegerField(default=0)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+    slug = models.SlugField(unique=True)
+    customers = models.ManyToManyField(
+        'UserProfile', through='Order', related_name='ordered_meals')
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.meal_name)
+        while not self.meal_id:
+            newid = ''.join([
+                    random.sample(string.letters, 2),
+                    random.sample(string.digits, 2),
+                    random.sample(string.letters, 2),
+            ])
+
+            if not self.objects.filter(pk=newid).exists():
+                self.meal_id = newid
+        super(Meal, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name_plural = 'Meals'
+
+    def __str__(self):
+        return self.meal_name
+
 
 class Restaurant(models.Model):
-	restaurant_id = models.IntegerField()
-	restaurant_name = models.CharField(max_length=30, unique=True)
-	type = models.CharField(max_length=30, unique=False)
-	average_rating = models.IntegerField()
-	
+    restaurant_id = models.IntegerField()
+    restaurant_name = models.CharField(max_length=30, unique=True)
+    type = models.CharField(max_length=30, unique=False)
+    average_rating = models.IntegerField()
+
+
 class Order(models.Model):
-	user_id = models.IntegerField()
-	meal_id = models.IntegerField()
-	date = models.DateTimeField()
-	
+    meal = models.ForeignKey(
+        'Meal', related_name='orders', on_delete=models.SET_NULL, null=True)
+    customer = models.ForeignKey('UserProfile', related_name='orders',
+                                 on_delete=models.SET_NULL, null=True, blank=True)
+    date = models.DateTimeField()
+
+
 class Favourite(models.Model):
-	user_id = models.IntegerField()
-	meal_id = models.IntegerField()
-	
+    user_id = models.IntegerField()
+    meal_id = models.IntegerField()
+
+
 class Rating(models.Model):
-	user_id = models.IntegerField()
-	restaurant_id = models.IntegerField()
-	rating = models.IntegerField()
-	
+    user_id = models.IntegerField()
+    restaurant_id = models.IntegerField()
+    rating = models.IntegerField()
+
 
 class Category(models.Model):
-	name = models.CharField(max_length=128, unique=True)
-	views = models.IntegerField(default=0)
-	likes = models.IntegerField(default=0)
-	slug = models.SlugField(unique=True)
-	
-	def save(self, *args, **kwargs):
-		self.slug = slugify(self.name)
-		super(Category, self).save(*args, **kwargs)
-	
-	class Meta:
-		verbose_name_plural = 'Categories'
-	
-	def __str__(self):
-		return self.name
+    name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
 
 class Page(models.Model):
-	TITLE_MAX_LENGTH = 128
-	URL_MAX_LENGTH = 200
+    TITLE_MAX_LENGTH = 128
+    URL_MAX_LENGTH = 200
 
-	category = models.ForeignKey(Category, on_delete=models.CASCADE)
-	title = models.CharField(max_length=TITLE_MAX_LENGTH)
-	url = models.URLField()
-	views = models.IntegerField(default=0)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=TITLE_MAX_LENGTH)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+        return self.title
--- food_cloud/views.py
+++ food_cloud/views.py
@@ -11,170 +11,197 @@ from django.contrib.auth.decorators import login_required
 from datetime import datetime
 from food_cloud.bing_search import run_query
 
+
 def index(request):
-	context_dict = {}
-	#context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
-	#context_dict['categories'] = category_list
-	#context = RequestContext(request)
-	category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {}
+    # context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    # context_dict['categories'] = category_list
+    # context = RequestContext(request)
+    category_list = Category.objects.order_by('-likes')[:5]
 
-	page_list = Page.objects.order_by('-views')[:5]
-	
-	meal_list = Meal.objects.order_by('meal_name')
-	
-	context_dict = {}
-	context_dict['categories'] = category_list
-	context_dict['pages'] = page_list
-	context_dict['meals'] = meal_list
-	
-	visitor_cookie_handler(request)
-	context_dict['visits'] = request.session['visits']
+    page_list = Page.objects.order_by('-views')[:5]
+    meal_list = Meal.objects.order_by('meal_name')
 
-	response = render(request, 'food_cloud/index.html', context=context_dict)
-	return response
+    context_dict = {}
+    context_dict['categories'] = category_list
+    context_dict['pages'] = page_list
+    context_dict['meals'] = meal_list
+    context_dict['orders'] = Order.objects.order_by('date')
+
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+
+    response = render(request, 'food_cloud/index.html', context=context_dict)
+    return response
+
 
 def about(request):
-	context_dict = {}
-	context_dict['boldmessage'] = 'This project was put together by Peter Rabai'
-	visitor_cookie_handler(request)
-	context_dict['visits'] = request.session['visits']
-	response = render(request, 'food_cloud/about.html', context=context_dict)
-	return response
-	
+    context_dict = {}
+    context_dict['boldmessage'] = 'This project was put together by Peter Rabai'
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    response = render(request, 'food_cloud/about.html', context=context_dict)
+    return response
+
+
 def contact(request):
-	context_dict = {}
-	context_dict['boldmessage'] = 'Contact us page(WIP)'
-	visitor_cookie_handler(request)
-	context_dict['visits'] = request.session['visits']
-	response = render(request, 'food_cloud/contact.html', context=context_dict)
-	return response
-	
+    context_dict = {}
+    context_dict['boldmessage'] = 'Contact us page(WIP)'
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    response = render(request, 'food_cloud/contact.html', context=context_dict)
+    return response
+
+
 def my_account(request):
-	context_dict = {}
-	context_dict['boldmessage'] = 'my_account(WIP)'
-	visitor_cookie_handler(request)
-	context_dict['visits'] = request.session['visits']
-	response = render(request, 'food_cloud/my_account.html', context=context_dict)
-	return response
-	
+    context_dict = {}
+    context_dict['boldmessage'] = 'my_account(WIP)'
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    response = render(request, 'food_cloud/my_account.html',
+                      context=context_dict)
+    return response
+
+
 def add_meal(request):
-	form = MealForm()
-	if request.method == 'POST':
-		form = MealForm(request.POST)
-		if form.is_valid():
-			form.save(commit=True)
-			return redirect('/food_cloud/')
-		else:
-			print(form.errors)
-	return render(request, 'food_cloud/add_meal.html', {'form': form})
-	
+    form = MealForm()
+    if request.method == 'POST':
+        form = MealForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect('/food_cloud/')
+        else:
+            print(form.errors)
+    return render(request, 'food_cloud/add_meal.html', {'form': form})
+
+
 def search(request):
-	result_list = []
-	query=""
-	if request.method == 'POST':
-		query = request.POST['query'].strip()
-		if query:
-			result_list = run_query(query)
-	print("DEBUG query=" + query)
-	return render(request, 'food_cloud/search.html', {'result_list': result_list, 'query': query})
-	
+    result_list = []
+    query = ""
+    if request.method == 'POST':
+        query = request.POST['query'].strip()
+        if query:
+            result_list = run_query(query)
+    print("DEBUG query=" + query)
+    return render(request, 'food_cloud/search.html', {'result_list': result_list, 'query': query})
+
+
 def show_meal(request, meal_name_slug):
-	context_dict = {}
-	try:
-		meal = Meal.objects.get(slug=meal_name_slug)
-		context_dict['meal'] = meal
-	except Meal.DoesNotExist:
-		context_dict['meal'] = None
-	return render(request, 'food_cloud/meal.html', context=context_dict)
-	
+    context_dict = {}
+    try:
+        meal = Meal.objects.get(slug=meal_name_slug)
+        context_dict['meal'] = meal
+    except Meal.DoesNotExist:
+        context_dict['meal'] = None
+    return render(request, 'food_cloud/meal.html', context=context_dict)
+
+
 def show_category(request, category_name_slug):
-	context_dict = {}
-	try:
-		category = Category.objects.get(slug=category_name_slug)
-		pages = Page.objects.filter(category=category)
-		context_dict['pages'] = pages
-		context_dict['category'] = category
-	except Category.DoesNotExist:
-		context_dict['category'] = None
-		context_dict['pages'] = None
-	return render(request, 'food_cloud/category.html', context=context_dict)
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+    return render(request, 'food_cloud/category.html', context=context_dict)
 
-@login_required	
+
+@login_required
 def add_category(request):
-	form = CategoryForm()
-	# A HTTP POST?
-	if request.method == 'POST':
-		form = CategoryForm(request.POST)
-		# Have we been provided with a valid form?
-		if form.is_valid():
-			# Save the new category to the database.
-			form.save(commit=True)
-			# Now that the category is saved, we could confirm this.
-			# For now, just redirect the user back to the index view.
-			return redirect('/food_cloud/')
-		else:
-			# The supplied form contained errors -
-			# just print them to the terminal.
-			print(form.errors)
-	# Will handle the bad form, new form, or no form supplied cases.
-	# Render the form with error messages (if any).
-	return render(request, 'food_cloud/add_category.html', {'form': form})
+    form = CategoryForm()
+    # A HTTP POST?
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        # Have we been provided with a valid form?
+        if form.is_valid():
+            # Save the new category to the database.
+            form.save(commit=True)
+            # Now that the category is saved, we could confirm this.
+            # For now, just redirect the user back to the index view.
+            return redirect('/food_cloud/')
+        else:
+            # The supplied form contained errors -
+            # just print them to the terminal.
+            print(form.errors)
+    # Will handle the bad form, new form, or no form supplied cases.
+    # Render the form with error messages (if any).
+    return render(request, 'food_cloud/add_category.html', {'form': form})
 
-@login_required	
+
+@login_required
 def add_page(request, category_name_slug):
-	try:
-		category = Category.objects.get(slug=category_name_slug)
-	except:
-		category = None
-	
-	# You cannot add a page to a Category that does not exist... DM
-	if category is None:
-		return redirect('/food_cloud/')
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+    except:
+        category = None
 
-	form = PageForm()
+    # You cannot add a page to a Category that does not exist... DM
+    if category is None:
+        return redirect('/food_cloud/')
 
-	if request.method == 'POST':
-		form = PageForm(request.POST)
+    form = PageForm()
 
-		if form.is_valid():
-			if category:
-				page = form.save(commit=False)
-				page.category = category
-				page.views = 0
-				page.save()
+    if request.method == 'POST':
+        form = PageForm(request.POST)
 
-				return redirect(reverse('food_cloud:show_category', kwargs={'category_name_slug': category_name_slug}))
+        if form.is_valid():
+            if category:
+                page = form.save(commit=False)
+                page.category = category
+                page.views = 0
+                page.save()
 
-				
-			else:
-				print(form.errors)
-	
-	context_dict = {'form': form, 'category': category}
-	return render(request, 'food_cloud/add_page.html', context=context_dict)
-	
+                return redirect(reverse('food_cloud:show_category', kwargs={'category_name_slug': category_name_slug}))
+
+            else:
+                print(form.errors)
+
+    context_dict = {'form': form, 'category': category}
+    return render(request, 'food_cloud/add_page.html', context=context_dict)
+
+
 @login_required
 def restricted(request):
-	return HttpResponse("Since you're logged in, you can see this text!")
+    return HttpResponse("Since you're logged in, you can see this text!")
+
 
 def get_server_side_cookie(request, cookie, default_val=None):
-	val = request.session.get(cookie)
-	if not val:
-		val = default_val
-	return val	
-	
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+
 def visitor_cookie_handler(request):
-	visits = int(get_server_side_cookie(request, 'visits', '1'))
-	last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
-	last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
-	
-	# If it's been more than a day since the last visit...
-	if (datetime.now() - last_visit_time).days > 0:
-		visits = visits + 1
-		# Update the last visit cookie now that we have updated the count
-		request.session['last_visit'] = str(datetime.now())
-	else:
-		# Set the last visit cookie
-		request.session['last_visit'] = last_visit_cookie
-	
-	# Update/set the visits cookie
-	request.session['visits'] = visits
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(
+        request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(
+        last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    # If it's been more than a day since the last visit...
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        # Update the last visit cookie now that we have updated the count
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        # Set the last visit cookie
+        request.session['last_visit'] = last_visit_cookie
+
+    # Update/set the visits cookie
+    request.session['visits'] = visits
+
+
+@login_required
+def add_order(request, meal_name_slug):
+    meal = Meal.objects.get(slug=meal_name_slug)
+    print(request.user.email, flush=True)
+
+    user = UserProfile.objects.get(user=request.user)
+    order = Order.objects.create(
+        meal=meal, customer=user, date=datetime.now())
+    order.save()
+    return index(request)
