66771dc02b31d4abfd4f588fd88e23a59df4ac5a
Peter Rabai <2394958r@student.gla.ac.uk>
2020-04-02 14:31:09 +0100

    new population script with more meals

--- populate.py
+++ populate.py
@@ -5,12 +5,29 @@ import django
 import random
 django.setup()
 from food_cloud.models import *
+from django.test import TestCase
+from django.contrib.auth.models import User
+from django.db import models
+from django.template.defaultfilters import slugify
+from food_cloud.models import *
+from django.template.defaultfilters import slugify
+from django.urls import reverse
 
 def populate():
 
-	meals = {'Test_meal_1': {'description': "Test_meal_1 description", 'price': 15, 'restaurant_slug': "cluckys", 'picture': "/static/images/meal_default.jpg", 'average_rating': 4, 'num_orders': 5}, 
-			'Test_meal_2': {'description': "Test_meal_2 description", 'price': 15, 'restaurant_slug': "cluckys", 'picture': "/static/images/meal_default.jpg", 'average_rating': 4.9, 'num_orders': 10},
-			'Test_meal_3': {'description': "Test_meal_3 description", 'price': 15, 'restaurant_slug': "cluckys", 'picture': "/static/images/meal_default.jpg", 'average_rating': 3.5, 'num_orders': 2}}
+	meals = {'Bacon Banana Pancakes': {'description': "Bacon banana pancakes", 'price': 15, 'restaurant_slug': "gourmet-dining", 'picture': "/static/images/bacon_banana_pancakes.jpeg", 'average_rating': 3.5, 'num_orders': 2},
+			'Blue Cheese Burger': {'description': "Blue Cheese Burger", 'price': 15, 'restaurant_slug': "burger-emporium", 'picture': "/static/images/blue_cheese_burger.jpg", 'average_rating': 3.5, 'num_orders': 2},
+			'Box of Donuts': {'description': "Box of Donuts", 'price': 15, 'restaurant_slug': "cluckys", 'picture': "/static/images/box_of_donuts.jpeg", 'average_rating': 3.5, 'num_orders': 2},
+			'Cabbage Bun Burger': {'description': "Test_meal_3 description", 'price': 15, 'restaurant_slug': "burger-emporium", 'picture': "/static/images/cabbage_bun_burger.jpg", 'average_rating': 3.5, 'num_orders': 2},
+			'Creamy Tuscan Shrimp Linguine': {'description': "Test_meal_3 description", 'price': 15, 'restaurant_slug': "gourmet-dining", 'picture': "/static/images/creamy_tuscan_shrimp_linguine.jpg", 'average_rating': 3.5, 'num_orders': 2},
+			'Dinosaur Chicken Nugget Pizza': {'description': "Test_meal_3 description", 'price': 15, 'restaurant_slug': "all-day-pizza", 'picture': "/static/images/dinosaur_chicken_nugget_pizza.jpeg", 'average_rating': 3.5, 'num_orders': 2},
+			'Fried Chicken Feet': {'description': "Test_meal_3 description", 'price': 15, 'restaurant_slug': "cluckys", 'picture': "/static/images/fried_chicken_feet.jpeg", 'average_rating': 3.5, 'num_orders': 2},
+			'Ham Banana Hollandaise': {'description': "Test_meal_3 description", 'price': 15, 'restaurant_slug': "gourmet-dining", 'picture': "/static/images/ham-banana-hollandaise.jpeg", 'average_rating': 3.5, 'num_orders': 2},
+			'Meatball Hotdog': {'description': "Test_meal_3 description", 'price': 15, 'restaurant_slug': "hotdogs-galore", 'picture': "/static/images/meatball_hotdog.jpeg", 'average_rating': 3.5, 'num_orders': 2},
+			'Pickle Dog': {'description': "Test_meal_3 description", 'price': 15, 'restaurant_slug': "hotdogs-galore", 'picture': "/static/images/pickle_dog.jpeg", 'average_rating': 3.5, 'num_orders': 2},
+			'Standard Burger': {'description': "Test_meal_3 description", 'price': 15, 'restaurant_slug': "all-day-pizza", 'picture': "/static/images/standard_burger.jpeg", 'average_rating': 3.5, 'num_orders': 2},
+			'Standard Hotdog': {'description': "Test_meal_3 description", 'price': 15, 'restaurant_slug': "hotdogs-galore", 'picture': "/static/images/standard_hotdog.jpg", 'average_rating': 3.5, 'num_orders': 2},
+			'Trex Burger': {'description': "Test_meal_3 description", 'price': 15, 'restaurant_slug': "burger-emporium", 'picture': "/static/images/trex_burger.jpeg", 'average_rating': 3.5, 'num_orders': 2}}
 
 	add_restaurant()
 
@@ -21,8 +38,17 @@ def populate():
 			print("Meal already exists")
 
 def add_restaurant():
-	r = RestaurantProfile.objects.get_or_create(user=create_user(), restaurant_name="Clucky's", type="Chicken")
-	return r
+	r1, created = RestaurantProfile.objects.get_or_create(user=User.objects.create_user(username="Clucky's"), restaurant_name="Clucky's", type="Chicken")
+	r2, created = RestaurantProfile.objects.get_or_create(user=User.objects.create_user(username="Burger Emporium"), restaurant_name="Burger Emporium", type="Burger")
+	r3, created = RestaurantProfile.objects.get_or_create(user=User.objects.create_user(username="All day pizza"), restaurant_name="All day pizza", type="Pizza")
+	r4, created = RestaurantProfile.objects.get_or_create(user=User.objects.create_user(username="Gourmet Dining"), restaurant_name="Gourmet Dining", type="Chicken")
+	r5, created = RestaurantProfile.objects.get_or_create(user=User.objects.create_user(username="Hotdogs Galore"), restaurant_name="Hotdogs Galore", type="Hotdog")
+	# r1 = RestaurantProfile.objects.get_or_create(user=create_user(), restaurant_name="Clucky's", type="Chicken")
+	# r2 = RestaurantProfile.objects.get_or_create(user=create_user(), restaurant_name="Burger Emporium", type="Burger")
+	# r3 = RestaurantProfile.objects.get_or_create(user=create_user(), restaurant_name="All day pizza", type="Pizza")
+	# r4 = RestaurantProfile.objects.get_or_create(user=create_user(), restaurant_name="Gourmet Dining", type="Chicken")
+	# r5 = RestaurantProfile.objects.get_or_create(user=create_user(), restaurant_name="Hotdogs Galore", type="Hotdog")
+	#return r
 
 def create_user():
 	try:
