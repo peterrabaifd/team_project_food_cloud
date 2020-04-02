f51d105ebc0fc048cc493fa9d798977db5afeb09
Peter Rabai <2394958r@student.gla.ac.uk>
2020-04-02 15:54:01 +0100

    settings change for static root

--- team_project_food_cloud/settings.py
+++ team_project_food_cloud/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [STATIC_DIR, MEDIA_DIR, ]
-STATIC_ROOT = /home/pkovacs92/team_project_food_cloud/static
+STATIC_ROOT = '/home/pkovacs92/team_project_food_cloud/static'
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 LOGIN_URL = 'food_cloud:login'
